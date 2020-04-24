807144e9e42e6e4634c9cabaec4de7db4e808353
Jakub Polak <jakubpolak95@gmail.com>
2020-04-24 22:55:10 +0200

    format code

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,7 +5,8 @@ fn main() {
     println!("Input your guess: ");
 
     let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
+    io::stdin()
+        .read_line(&mut guess)
         .expect("Failed to read line.");
 
     println!("You guessed: {}", guess)
