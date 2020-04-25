a9165c4947c0f525d2437f09315214554be2a93b
Jakub Polak <jakubpolak95@gmail.com>
2020-04-26 00:58:46 +0200

    add variables

---
+++ variables/target/debug/deps/variables-b295febe3bb60fe1.d
@@ -0,0 +1,5 @@
+/home/jap2/home/rust/variables/target/debug/deps/variables-b295febe3bb60fe1: src/main.rs
+
+/home/jap2/home/rust/variables/target/debug/deps/variables-b295febe3bb60fe1.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/rls/debug/deps/variables-f5e41a8ff3e968e1.d
@@ -0,0 +1,5 @@
+/home/jap2/home/rust/variables/target/rls/debug/deps/variables-f5e41a8ff3e968e1.rmeta: src/main.rs
+
+/home/jap2/home/rust/variables/target/rls/debug/deps/variables-f5e41a8ff3e968e1.d: src/main.rs
+
+src/main.rs:
