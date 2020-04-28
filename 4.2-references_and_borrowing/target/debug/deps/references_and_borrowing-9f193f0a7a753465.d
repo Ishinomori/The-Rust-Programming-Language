4a6f2330e72f6d1d8396f3f3d15c110f0ae28565
Jakub Polak <jakubpolak95@gmail.com>
2020-04-28 19:44:35 +0200

    clean-up

--- 4.2-references_and_borrowing/target/debug/deps/references_and_borrowing-9f193f0a7a753465.d
+++
@@ -1,5 +0,0 @@
-/home/jap2/home/rust/4.2-references_and_borrowing/target/debug/deps/references_and_borrowing-9f193f0a7a753465: src/main.rs
-
-/home/jap2/home/rust/4.2-references_and_borrowing/target/debug/deps/references_and_borrowing-9f193f0a7a753465.d: src/main.rs
-
-src/main.rs:
