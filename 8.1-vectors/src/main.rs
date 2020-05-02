095790836dacb6e217a86dd825048d9197ddbc3b
Jakub Polak <jakubpolak95@gmail.com>
2020-05-03 01:18:40 +0200

    add vectors chapter

---
+++ 8.1-vectors/src/main.rs
@@ -0,0 +1,52 @@
+fn main() {
+    // empty vector
+    let v: Vec<i32> = Vec::new();
+
+    // vector macro
+    let v = vec![1, 2, 3];
+
+    // updating a vector
+    let mut v = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+
+    // reading elements
+    let v = vec![1, 2, 3, 4, 5];
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    }
+
+    let v = vec![1, 2, 3, 4, 5];
+
+    // let does_not_exist = &v[100]; panic!
+    let does_not_exist = v.get(100);
+
+    // iterating over vector
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{}", i);
+    }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
+
+    // enum to store multiple types
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+}
